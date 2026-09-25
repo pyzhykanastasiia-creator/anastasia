2dfdb440a9d16dc0ce2a9380a8d51e90e59a0e84
pyzhykanastasiia-creator <pyzhykanastasiia@gmail.com>
2026-09-25 22:15:35 +0200

    Update SomeOf.tsx

--- src/components/SomeOf/SomeOf.tsx
+++ src/components/SomeOf/SomeOf.tsx
@@ -25,7 +25,7 @@ const config = {
     },
     {
       title: 'Hairstyle 3',
-      image: '/img/homepage/Half up half down hairstyle.JPG',
+      image: '/img/homepage/Halfuphalfdownhairstyle.JPG',
     },
   ]
 };
